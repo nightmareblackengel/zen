0fe87a1e6d0250625e3e9654cc99549e975a7ddc
black_engel <nbe.samsung.j710fn@gmail.com>
2024-12-18 16:57:56 +0200

    testing Environment

--- src/Config/AbstractConfiguration.php
+++ src/Config/AbstractConfiguration.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace ZenPaymentSdk\Config;
 
+use ZenPaymentSdk\DotEnv;
+
 abstract class AbstractConfiguration
 {
     public string $apiUri;
@@ -13,4 +15,13 @@ abstract class AbstractConfiguration
     public string $ipnServer = 'https://ipn-pay.g2a.com/ipn';
 
     public string $ipnServerIp = '104.22.63.240';
+
+    public function __construct()
+    {
+        define("ROOT_PATH", dirname(dirname(__DIR__)));
+
+        var_dump(ROOT_PATH);
+        $dotEnv = new DotEnv();
+        $dotEnv->load(ROOT_PATH . '/.env');
+    }
 }
