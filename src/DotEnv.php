0f20610caae10938ef0a9d590f910b827425b878
black_engel <nbe.samsung.j710fn@gmail.com>
2024-12-18 17:05:03 +0200

    testing Environment

--- src/DotEnv.php
+++ src/DotEnv.php
@@ -8,6 +8,10 @@ class DotEnv
 {
     public function load(string $path): void
     {
+        if (!file_exists($path)) {
+            return;
+        }
+
         $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
         foreach ($lines as $line) {
